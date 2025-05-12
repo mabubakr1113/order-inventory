4813bc978ef25eb4c002ab39a8d13108f53f60e9
mabubakr1113 <mohammadabubakr1113@gmail.com>
2025-05-12 17:42:51 +0500

    feat: add nestjs/config

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -2,13 +2,22 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { JwtStrategy } from './jwt.strategy';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
     PassportModule,
-    JwtModule.register({
-      secret: 'secretKey',
-      signOptions: { expiresIn: '1h' },
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        secret:
+          configService.get<string>('JWT_SECRET') ||
+          'a-string-secret-at-least-256-bits-long',
+        signOptions: {
+          expiresIn: configService.get<string>('JWT_EXPIRES_IN') || '1h',
+        },
+      }),
     }),
   ],
   providers: [JwtStrategy],
--- src/auth/jwt.strategy.ts
+++ src/auth/jwt.strategy.ts
@@ -1,18 +1,21 @@
 import { Injectable } from '@nestjs/common';
 import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
-  constructor() {
+  constructor(configService: ConfigService) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       ignoreExpiration: false,
-      secretOrKey: 'secretKey',
+      secretOrKey:
+        configService.get<string>('JWT_SECRET') ||
+        'a-string-secret-at-least-256-bits-long',
     });
   }
 
-  async validate(payload: any) {
+  validate(payload: { sub: string; username: string }) {
     return { userId: payload.sub, username: payload.username };
   }
 }
