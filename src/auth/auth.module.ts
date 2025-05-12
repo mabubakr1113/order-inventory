919e642c74028e44f3be0f04144a7c703921d7e0
mabubakr1113 <mohammadabubakr1113@gmail.com>
2025-05-12 15:48:53 +0500

    feat: add authentication module with JWT strategy

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,16 @@
+import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
+import { JwtStrategy } from './jwt.strategy';
+
+@Module({
+  imports: [
+    PassportModule,
+    JwtModule.register({
+      secret: 'secretKey',
+      signOptions: { expiresIn: '1h' },
+    }),
+  ],
+  providers: [JwtStrategy],
+})
+export class AuthModule {}
